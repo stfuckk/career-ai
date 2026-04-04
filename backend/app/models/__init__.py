81572a394cec7a5fd48ac7ade2b23d013390b503
restupkolpp-source <restupkolpp@gmail.com>
2026-04-04 17:10:18 +0700

    исправил ответ от нейронки

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,3 +1,4 @@
+from app.models.ai_recommendation_job import AIJobStatusEnum, AIRecommendationJob
 from app.models.career_recommendation import CareerRecommendation
 from app.models.career_test_attempt import CareerTestAttempt
 from app.models.career_test_score import CareerTestScore
@@ -6,6 +7,8 @@ from app.models.user import User
 from app.models.vacancy_recommendation import VacancyRecommendation
 
 __all__ = [
+    'AIJobStatusEnum',
+    'AIRecommendationJob',
     'User',
     'TestMethodology',
     'CareerTestAttempt',
