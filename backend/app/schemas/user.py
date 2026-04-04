c6ba20e751be8270f9c78884b4fc01982459b2d0
restupkolpp-source <restupkolpp@gmail.com>
2026-04-04 11:28:20 +0700

    add new fields to user

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,14 +1,17 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, Field
 
 from app.schemas.common import TimestampedRead
 
 
 class UserRead(TimestampedRead):
     id: int
-    email: EmailStr
-    full_name: str | None
+    login: str
     is_active: bool
+    sex: str
+    education_level: str
+    work_experience: int | None
+    is_first_time_completing_test: bool
 
 
 class UserUpdateRequest(BaseModel):
-    full_name: str | None = Field(default=None, max_length=255)
+    login: str = Field(max_length=255)
